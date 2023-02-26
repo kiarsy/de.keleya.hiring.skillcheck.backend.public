cea085c1a51288485cceb92e10a39984e73045a3
kiarsy <kiarsy@yahoo.com>
2023-02-26 11:24:33 +0400

    add service unit test

--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,32 +1,74 @@
-import { ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
-import { Test, TestingModule } from '@nestjs/testing';
-import { JwtStrategy } from '../common/strategies/jwt.strategy';
+import { NotFoundException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from '../prisma.services';
 import { UserService } from './user.service';
+jest.mock('../prisma.services');
 
 describe('UserService', () => {
   let userService: UserService;
 
+  const mockPrismaService = {
+    $queryRaw: jest.fn(),
+    $transaction: jest.fn(),
+    user: {
+      findMany: jest.fn(),
+      findFirst: jest.fn(),
+      update: jest.fn(),
+    },
+    credentials: {
+      create: jest.fn(),
+    },
+  };
+
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      imports: [
-        PassportModule,
-        JwtModule.register({
-          secret: 'JWT_SECRET',
-          signOptions: {
-            expiresIn: '1year',
-            algorithm: 'HS256',
-          },
-        }),
-      ],
-      providers: [UserService, PrismaService, JwtStrategy, ConfigService],
-    }).compile();
-    userService = module.get<UserService>(UserService);
+    userService = new UserService(mockPrismaService as any, new JwtService({}));
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 
   it('should be defined', () => {
+    console.log('userService;', userService);
     expect(userService).toBeDefined();
+  });
+
+  describe('findUnique', () => {
+    it('should return a user when it exists and is not deleted', async () => {
+      const mockWhereUnique = { id: 1 };
+      const mockUser = { id: 1, name: 'John', email: 'john@example.com' };
+      mockPrismaService.user.findFirst.mockResolvedValue(mockUser);
+
+      const result = await userService.findUnique(mockWhereUnique, true);
+
+      expect(result).toEqual(mockUser);
+      expect(mockPrismaService.user.findFirst).toHaveBeenCalledWith({
+        where: {
+          id: mockWhereUnique.id,
+          is_deleted: false,
+        },
+        include: {
+          credential: true,
+        },
+      });
+    });
+
+    it('should reject with NotFoundException when the user does not exist', async () => {
+      const mockWhereUnique = { id: 1 };
+      mockPrismaService.user.findFirst.mockResolvedValue(null);
+
+      await expect(userService.findUnique(mockWhereUnique)).rejects.toEqual(
+        new NotFoundException(undefined, 'User Not found'),
+      );
+      expect(mockPrismaService.user.findFirst).toHaveBeenCalledWith({
+        where: {
+          id: mockWhereUnique.id,
+          is_deleted: false,
+        },
+        include: {
+          credential: false,
+        },
+      });
+    });
   });
 });
