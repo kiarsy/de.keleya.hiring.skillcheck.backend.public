2f263812e398e13064cf4832d308c6763bb43ccd
kiarsy <kiarsy@yahoo.com>
2023-02-26 13:03:33 +0400

    refactor hashpassword

--- src/common/utils/password.ts
+++ src/common/utils/password.ts
@@ -4,14 +4,13 @@ const saltOrRounds = 10;
 export class HashPassword {
   static async hashPassword(password: string): Promise<string> {
     return bcrypt.hash(password, saltOrRounds);
-  };
+  }
 
-  static  hashPasswordSync(password: string): string {
+  static hashPasswordSync(password: string): string {
     return bcrypt.hashSync(password, saltOrRounds);
-  };
+  }
 
-  static async matchHashedPassword(password: string, hash: string): Promise<boolean> {
-    return bcrypt.compare(password, hash);
-  };
-
+  static matchHashedPassword(password: string, hash: string): boolean {
+    return bcrypt.compareSync(password, hash);
+  }
 }
